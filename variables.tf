d32712dbb68b3f30c5f9c71192144b7ebaab50a1
aknysh <aknysh>
2017-09-01 16:06:33 -0400

    Add description to `variables.tf`

--- variables.tf
+++ variables.tf
@@ -39,25 +39,30 @@ variable "tags" {
 }
 
 variable "privileged_mode" {
-  default = false
+  default     = false
+  description = "(Optional, used when building Docker images). If set to true, enables running the Docker daemon inside a Docker container."
 }
 
 variable "aws_region" {
-  type    = "string"
-  default = ""
+  type        = "string"
+  default     = ""
+  description = "(Optional, used when building Docker images and pushing them to ECR). AWS Region for the ECR repository, e.g. us-east-1. For more info: http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html"
 }
 
 variable "aws_account_id" {
-  type    = "string"
-  default = ""
+  type        = "string"
+  default     = ""
+  description = "(Optional, used when building Docker images and pushing them to ECR). AWS Account ID that owns the ECR repository. For more info: http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html"
 }
 
 variable "image_repo_name" {
-  type    = "string"
-  default = ""
+  type        = "string"
+  default     = ""
+  description = "(Optional, used when building Docker images and pushing them to ECR). ECR repository name to store Docker images. For more info: http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html"
 }
 
 variable "image_tag" {
-  type    = "string"
-  default = ""
+  type        = "string"
+  default     = ""
+  description = "(Optional, used when building Docker images and pushing them to ECR). Docker image tag in the ECR repository, e.g. 'latest'. For more info: http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html"
 }
