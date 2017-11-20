4477173dc459c3586d19f6013bf1ccaed50a75a4
Igor Rodionov <goruha@gmail.com>
2017-11-20 17:00:42 +0600

    Fix codebuild

--- main.tf
+++ main.tf
@@ -86,25 +86,24 @@ resource "aws_codebuild_project" "default" {
     type            = "LINUX_CONTAINER"
     privileged_mode = "${var.privileged_mode}"
 
-    environment_variable {
-      "name"  = "AWS_REGION"
-      "value" = "${signum(length(var.aws_region)) == 1 ? var.aws_region : data.aws_region.default.name}"
-    }
-
-    environment_variable {
-      "name"  = "AWS_ACCOUNT_ID"
-      "value" = "${signum(length(var.aws_account_id)) == 1 ? var.aws_account_id : data.aws_caller_identity.default.account_id}"
-    }
-
-    environment_variable {
-      "name"  = "IMAGE_REPO_NAME"
-      "value" = "${signum(length(var.image_repo_name)) == 1 ? var.image_repo_name : "UNSET"}"
-    }
-
-    environment_variable {
-      "name"  = "IMAGE_TAG"
-      "value" = "${signum(length(var.image_tag)) == 1 ? var.image_tag : "latest"}"
-    }
+    environment_variable = [
+      {
+        "name"  = "AWS_REGION"
+        "value" = "${signum(length(var.aws_region)) == 1 ? var.aws_region : data.aws_region.default.name}"
+      },
+      {
+        "name"  = "AWS_ACCOUNT_ID"
+        "value" = "${signum(length(var.aws_account_id)) == 1 ? var.aws_account_id : data.aws_caller_identity.default.account_id}"
+      },
+      {
+        "name"  = "IMAGE_REPO_NAME"
+        "value" = "${signum(length(var.image_repo_name)) == 1 ? var.image_repo_name : "UNSET"}"
+      },
+      {
+        "name"  = "IMAGE_TAG"
+        "value" = "${signum(length(var.image_tag)) == 1 ? var.image_tag : "latest"}"
+      }
+    ]
   }
 
   source {
