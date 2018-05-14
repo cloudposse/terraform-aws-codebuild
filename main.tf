0fb28ea23c29011a3436d41ee2ffdace385714c0
Jamie Nelson <jamie@theinstillery.com>
2018-05-14 17:14:20 +0100

    added git:: to source

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ data "aws_region" "default" {}
 
 # Define composite variables for resources
 module "label" {
-  source     = "github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.1.0"
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.1.0"
   namespace  = "${var.namespace}"
   name       = "${var.name}"
   stage      = "${var.stage}"
