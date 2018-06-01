b0e5d5a5b01b3af5858e0bd1f0cc551d18731b24
Jamie Nelson <stack@bitflight.io>
2018-06-01 02:51:19 +0100

    Added an S3 cache, so that the buildspec.yml can use cache and dynamic environment vars. (#16)
    * Added s3 caching option, with autodelete cache
    
    * Added s3 caching option, with autodelete cache
    
    * Added s3 caching option, with autodelete cache
    
    * fixed error on conditional cache usage
    
    * Updated the cache_enabled option for clarity
    Added the ability to dynamically add environment variables.
    Set the s3 bucket to only create if the cache is enabled
    Added a null provider verion to remove the warning.
    Added a random provider with version to remove warning.
    Added a random string generator to append to bucket names to add uniqueness, lowering the chance of bucket name conflicts
    
    * removed build harness file
    
    * updated the environment_variables to use a default list containing a map. When it was empty, it caused an error
    
    * Fixed formatting for lint
    
    * updated variable description
    
    * fixed format
    
    * fixed source of label module
    
    * fixed source of label module
    
    * fixed source of label module
    
    * set label to use terraform-null-label
    
    * Switched to a tagged release of terraform-terraform-label
    
    * added git:: to source
    
    * moved provider section out of module, and added a section to README to make users aware of the providers used, nd how to squash the warning

--- main.tf
+++ main.tf
@@ -4,13 +4,64 @@ data "aws_region" "default" {}
 
 # Define composite variables for resources
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.1"
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.1.0"
   namespace  = "${var.namespace}"
   name       = "${var.name}"
   stage      = "${var.stage}"
   delimiter  = "${var.delimiter}"
   attributes = "${var.attributes}"
   tags       = "${var.tags}"
+}
+
+resource "aws_s3_bucket" "cache_bucket" {
+  count         = "${var.cache_enabled == "true" ? 1 : 0}"
+  bucket        = "${local.cache_bucket_name_normalised}"
+  acl           = "private"
+  force_destroy = true
+  tags          = "${module.label.tags}"
+
+  lifecycle_rule {
+    id      = "codebuildcache"
+    enabled = true
+
+    prefix = "/"
+    tags   = "${module.label.tags}"
+
+    expiration {
+      days = "${var.cache_expiration_days}"
+    }
+  }
+}
+
+resource "random_string" "bucket_prefix" {
+  length  = 12
+  number  = false
+  upper   = false
+  special = false
+  lower   = true
+}
+
+locals {
+  cache_bucket_name = "${module.label.id}${var.cache_bucket_suffix_enabled == "true" ? "-${random_string.bucket_prefix.result}" : "" }"
+
+  ## Clean up the bucket name to use only hyphens, and trim its length to 63 characters.
+  ## As per https://docs.aws.amazon.com/AmazonS3/latest/dev/BucketRestrictions.html
+  cache_bucket_name_normalised = "${substr(join("-", split("_", lower(local.cache_bucket_name))), 0, min(length(local.cache_bucket_name),63))}"
+
+  ## This is the magic where a map of a list of maps is generated
+  ## and used to conditionally add the cache bucket option to the 
+  ## aws_codebuild_project
+  cache_def = {
+    "true" = [{
+      type     = "S3"
+      location = "${var.cache_enabled == "true" ? aws_s3_bucket.cache_bucket.0.bucket : "none" }"
+    }]
+
+    "false" = []
+  }
+
+  # Final Map Selected from above
+  cache = "${local.cache_def[var.cache_enabled]}"
 }
 
 resource "aws_iam_role" "default" {
@@ -41,6 +92,13 @@ resource "aws_iam_policy" "default" {
   policy = "${data.aws_iam_policy_document.permissions.json}"
 }
 
+resource "aws_iam_policy" "default_cache_bucket" {
+  count  = "${var.cache_enabled == "true" ? 1 : 0}"
+  name   = "${module.label.id}-cache-bucket"
+  path   = "/service-role/"
+  policy = "${data.aws_iam_policy_document.permissions_cache_bucket.json}"
+}
+
 data "aws_iam_policy_document" "permissions" {
   statement {
     sid = ""
@@ -65,8 +123,31 @@ data "aws_iam_policy_document" "permissions" {
   }
 }
 
+data "aws_iam_policy_document" "permissions_cache_bucket" {
+  statement {
+    sid = ""
+
+    actions = [
+      "s3:*",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "${aws_s3_bucket.cache_bucket.arn}",
+      "${aws_s3_bucket.cache_bucket.arn}/*",
+    ]
+  }
+}
+
 resource "aws_iam_role_policy_attachment" "default" {
   policy_arn = "${aws_iam_policy.default.arn}"
+  role       = "${aws_iam_role.default.id}"
+}
+
+resource "aws_iam_role_policy_attachment" "default_cache_bucket" {
+  count      = "${var.cache_enabled == "true" ? 1 : 0}"
+  policy_arn = "${element(aws_iam_policy.default_cache_bucket.*.arn, count.index)}"
   role       = "${aws_iam_role.default.id}"
 }
 
@@ -78,41 +159,41 @@ resource "aws_codebuild_project" "default" {
     type = "CODEPIPELINE"
   }
 
+  # The cache as a list with a map object inside.
+  cache = ["${local.cache}"]
+
   environment {
     compute_type    = "${var.build_compute_type}"
     image           = "${var.build_image}"
     type            = "LINUX_CONTAINER"
     privileged_mode = "${var.privileged_mode}"
 
-    environment_variable {
+    environment_variable = [{
       "name"  = "AWS_REGION"
       "value" = "${signum(length(var.aws_region)) == 1 ? var.aws_region : data.aws_region.default.name}"
-    }
-
-    environment_variable {
-      "name"  = "AWS_ACCOUNT_ID"
-      "value" = "${signum(length(var.aws_account_id)) == 1 ? var.aws_account_id : data.aws_caller_identity.default.account_id}"
-    }
-
-    environment_variable {
-      "name"  = "IMAGE_REPO_NAME"
-      "value" = "${signum(length(var.image_repo_name)) == 1 ? var.image_repo_name : "UNSET"}"
-    }
-
-    environment_variable {
-      "name"  = "IMAGE_TAG"
-      "value" = "${signum(length(var.image_tag)) == 1 ? var.image_tag : "latest"}"
-    }
-
-    environment_variable {
-      "name"  = "STAGE"
-      "value" = "${var.stage}"
-    }
-
-    environment_variable {
-      "name"  = "GITHUB_TOKEN"
-      "value" = "${var.github_token}"
-    }
+    },
+      {
+        "name"  = "AWS_ACCOUNT_ID"
+        "value" = "${signum(length(var.aws_account_id)) == 1 ? var.aws_account_id : data.aws_caller_identity.default.account_id}"
+      },
+      {
+        "name"  = "IMAGE_REPO_NAME"
+        "value" = "${signum(length(var.image_repo_name)) == 1 ? var.image_repo_name : "UNSET"}"
+      },
+      {
+        "name"  = "IMAGE_TAG"
+        "value" = "${signum(length(var.image_tag)) == 1 ? var.image_tag : "latest"}"
+      },
+      {
+        "name"  = "STAGE"
+        "value" = "${signum(length(var.stage)) == 1 ? var.stage : "UNSET"}"
+      },
+      {
+        "name"  = "GITHUB_TOKEN"
+        "value" = "${signum(length(var.github_token)) == 1 ? var.github_token : "UNSET"}"
+      },
+      "${var.environment_variables}",
+    ]
   }
 
   source {
--- variables.tf
+++ variables.tf
@@ -10,6 +10,32 @@ variable "name" {
   default = "codebuild"
 }
 
+variable "environment_variables" {
+  type = "list"
+
+  default = [{
+    "name"  = "NO_ADDITIONAL_BUILD_VARS"
+    "value" = "TRUE"
+  }]
+
+  description = "A list of maps, that contain both the key 'name' and the key 'value' to be used as additional environment variables for the build."
+}
+
+variable "cache_enabled" {
+  default     = "true"
+  description = "If cache_enabled is true, create an S3 bucket for storing codebuild cache inside"
+}
+
+variable "cache_expiration_days" {
+  default     = "7"
+  description = "How many days should the build cache be kept."
+}
+
+variable "cache_bucket_suffix_enabled" {
+  default     = "true"
+  description = "The cache bucket generates a random 13 character string to generate a unique bucket name. If set to false it uses terraform-null-label's id value"
+}
+
 variable "build_image" {
   default     = "aws/codebuild/docker:1.12.1"
   description = "Docker image for build environment, e.g. 'aws/codebuild/docker:1.12.1' or 'aws/codebuild/eb-nodejs-6.10.0-amazonlinux-64:4.0.0'. For more info: http://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref.html"
