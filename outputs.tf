ce271cf596c3444cd520e0715399111881a3466b
Sarkis Varozian <svarozian@gmail.com>
2018-06-18 15:59:51 -0700

    use proper outputs for count

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "project_name" {
-  value = "${aws_codebuild_project.default.*.name}"
+  value = ["${aws_codebuild_project.default.*.name}"]
 }
 
 output "project_id" {
-  value = "${aws_codebuild_project.default.*.id}"
+  value = ["${aws_codebuild_project.default.*.id}"]
 }
 
 output "role_arn" {
-  value = "${aws_iam_role.default.*.id}"
+  value = ["${aws_iam_role.default.*.id}"]
 }
 
 output "cache_bucket_name" {
