3f0528f9f211bfd559118b3f4f6d8b14ef3fab11
Sarkis Varozian <svarozian@gmail.com>
2018-06-18 17:09:47 -0700

    Add enabled flag (#17)
    * add enabled flag

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "project_name" {
-  value = "${aws_codebuild_project.default.name}"
+  value = "${join("", aws_codebuild_project.default.*.name)}"
 }
 
 output "project_id" {
-  value = "${aws_codebuild_project.default.id}"
+  value = "${join("", aws_codebuild_project.default.*.id)}"
 }
 
 output "role_arn" {
-  value = "${aws_iam_role.default.id}"
+  value = "${join("", aws_iam_role.default.*.id)}"
 }
 
 output "cache_bucket_name" {
-  value = "${var.cache_enabled == "true" ? aws_s3_bucket.cache_bucket.0.bucket : "UNSET" }"
+  value = "${var.enabled == "true" && var.cache_enabled == "true" ? join("", aws_s3_bucket.cache_bucket.*.bucket) : "UNSET" }"
 }
