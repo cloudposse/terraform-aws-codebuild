dc75a8c607052c7626b18b84ba8077447ae0ba8b
Theo Chatzimichos <tampakrap@gmail.com>
2019-07-02 12:29:18 +0200

    Add codecommit:GitPull policy so that CodeBuild can be used against CodeCommit

--- main.tf
+++ main.tf
@@ -113,6 +113,7 @@ data "aws_iam_policy_document" "permissions" {
     sid = ""
 
     actions = [
+      "codecommit:GitPull",
       "ecr:BatchCheckLayerAvailability",
       "ecr:CompleteLayerUpload",
       "ecr:GetAuthorizationToken",
