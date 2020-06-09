461c87aac74bee66fb26ec637f768f180f35fc38
Bircan Bilici <bircanbilici@gmail.com>
2020-06-09 19:32:25 +0300

    added secret manager access

--- main.tf
+++ main.tf
@@ -136,6 +136,7 @@ data "aws_iam_policy_document" "permissions" {
       "logs:CreateLogStream",
       "logs:PutLogEvents",
       "ssm:GetParameters",
+      "secretsmanager:GetSecretValue",
     ]
 
     effect = "Allow"
