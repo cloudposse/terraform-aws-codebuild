55d0d2b6c1f391ade85de61a3817b29d2761fa21
Cfir Caemeli <cfir@dome9.com>
2021-01-31 20:20:53 +0200

    CR Fixes

--- main.tf
+++ main.tf
@@ -281,18 +281,17 @@ resource "aws_codebuild_project" "default" {
   }
 
   dynamic "secondary_sources"{
-      iterator = secondary_source
-      for_each = length(var.secondary_sources) > 0 ? var.secondary_sources : []
+      for_each = var.secondary_sources
       content {
-        git_clone_depth = secondary_source.value.git_clone_depth
-        location = secondary_source.value.location
+        git_clone_depth = lookup(secondary_source.value, git_clone_depth, null)
+        location = lookup(secondary_source.value,location, null)
         source_identifier = secondary_source.value.source_identifier
         type = secondary_source.value.type
-        insecure_ssl = secondary_source.value.insecure_ssl
-        report_build_status =  secondary_source.value.report_build_status
+        insecure_ssl = lookup(secondary_source.value, insecure_ssl, null)
+        report_build_status =  lookup(secondary_source.value, report_build_status, null)
 
         git_submodules_config {
-          fetch_submodules = secondary_source.value.fetch_submodules
+          fetch_submodules = lookup(secondary_source.value, fetch_submodules, null)
         }
       }
   }
