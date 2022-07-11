d74b938cba4c1f754a05d019abc1b4a4d6e351cc
Denis Simonovski <denis.simonovski@rackspace.com>
2022-07-11 15:47:42 +0200

    update terraform version to >= 1.0 and aws version in examples

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.0"
+      version = ">= 4.22.0"
     }
     random = {
       source  = "hashicorp/random"
