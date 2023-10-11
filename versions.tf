82c118847f2944ed5a3093e960236cad7cf89dde
Hrvoje Kusanic <hrvoje.kusanic@crowdhouse.ch>
2023-10-11 16:17:11 +0200

    fixed version

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.2.0"
+      version = ">= 5.20.0"
     }
     random = {
       source  = "hashicorp/random"
